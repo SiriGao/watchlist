20d5ca0e61763320355f302629125b0041f992b6
SiriGao <1297960899@qq.com>
2023-11-08 02:22:07 +0800

    Create, edit and delete item by form

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask
-from flask import url_for
+from flask import Flask,request,url_for,redirect,flash
 from flask_sqlalchemy import SQLAlchemy # 导入扩展类
 import os
 import sys
@@ -16,6 +15,7 @@ else: # 否则使用四个斜线
 app = Flask(__name__)#通过实例化这个类，创建一个程序对象app
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # 关闭对模型修改的监控
+app.config['SECRET_KEY'] = 'dev'  # 等同于 app.secret_key = 'dev'
 # 在扩展类实例化前加载配置
 db = SQLAlchemy(app) # 初始化扩展，传入程序实例 app
 
@@ -23,7 +23,6 @@ db = SQLAlchemy(app) # 初始化扩展，传入程序实例 app
 class User(db.Model):  # 表名将会是 user（自动生成，小写处理）
     id = db.Column(db.Integer, primary_key=True)  # 主键
     name = db.Column(db.String(20))  # 名字
-
 
 class Movie(db.Model):  # 表名将会是 movie
     id = db.Column(db.Integer, primary_key=True)  # 主键
@@ -40,9 +39,6 @@ def initdb(drop):
         db.drop_all()
     db.create_all()
     click.echo('Initialized database.')  # 输出提示信息
-
-
-
 
 
 #Web程序：编写不同的函数处理对应URL的请求
@@ -63,23 +59,6 @@ def test_url_for():
  # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
  print(url_for('test_url_for', num=2)) # 输出:/test?num=2 
  return 'Test page'
-
-'''
-#定义虚拟数据
-name = 'Grey Li'
-movies = [
-    {'title': 'My Neighbor Totoro', 'year': '1988'},
-    {'title': 'Dead Poets Society', 'year': '1989'},
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1999'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
-]
-'''
 
 
 #创建自定义命令 forge
@@ -125,10 +104,52 @@ def page_not_found(e):  # 接受异常对象作为参数
     return render_template('404.html'), 404  # 返回模板和状态码
 
 #返回渲染好的模板作为响应
-
-
-# ...
-@app.route('/') #装饰器，为函数绑定对应URL，当用户访问URL，触发此函数，获得返回值，并显示
+#创建电影条目
+@app.route('/',methods=['GET','POST']) #装饰器，为函数绑定对应URL，当用户访问URL，触发此函数，获得返回值，并显示
 def index():
+    if request.method == 'POST':  # 判断是否是 POST 请求
+        # 获取表单数据
+        title = request.form.get('title')  # 传入表单对应输入字段的 name 值
+        year = request.form.get('year')
+        # 验证数据
+        if not title or not year or len(year) > 4 or len(title) > 60:
+            flash('Invalid input.')  # 显示错误提示
+            return redirect(url_for('index'))  # 重定向回主页
+        # 保存表单数据到数据库
+        movie = Movie(title=title, year=year)  # 创建记录
+        db.session.add(movie)  # 添加到数据库会话
+        db.session.commit()  # 提交数据库会话
+        flash('Item created.')  # 显示成功创建的提示
+        return redirect(url_for('index'))  # 重定向回主页
+
     movies = Movie.query.all() # 读取所有电影记录
     return render_template('index.html', movies=movies)
+
+#编辑电影条目
+@app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
+def edit(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+
+    if request.method == 'POST':  # 处理编辑表单的提交请求
+        title = request.form['title']
+        year = request.form['year']
+
+        if not title or not year or len(year) != 4 or len(title) > 60:
+            flash('Invalid input.')
+            return redirect(url_for('edit', movie_id=movie_id))  # 重定向回对应的编辑页面
+
+        movie.title = title  # 更新标题
+        movie.year = year  # 更新年份
+        db.session.commit()  # 提交数据库会话
+        flash('Item updated.')
+        return redirect(url_for('index'))  # 重定向回主页
+
+    return render_template('edit.html', movie=movie)  # 传入被编辑的电影记录
+
+@app.route('/movie/delete/<int:movie_id>', methods=['POST'])  # 限定只接受 POST 请求
+def delete(movie_id):
+    movie = Movie.query.get_or_404(movie_id)  # 获取电影记录
+    db.session.delete(movie)  # 删除对应的记录
+    db.session.commit()  # 提交数据库会话
+    flash('Item deleted.')
+    return redirect(url_for('index'))  # 重定向回主页
