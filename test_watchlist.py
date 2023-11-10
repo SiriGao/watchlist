d98109f991a6e20ecd079d3730ce965cc0acd875
SiriGao <1297960899@qq.com>
2023-11-10 08:26:08 +0800

    Orignize application with package

--- test_watchlist.py
+++ test_watchlist.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 import unittest
 
-from app import app, db, Movie, User, forge, initdb
+#from app import app, db, Movie, User, forge, initdb
+from watchlist import app, db
+from watchlist.models import Movie, User
+from watchlist.commands import forge, initdb
 
 
 
